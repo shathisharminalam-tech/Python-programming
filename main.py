e9247befe24e7cf766d71054756165e0b802818c
shathisharminalam-tech <shathisharminalam@gmail.com>
2026-04-28 23:38:03 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import turtle
+total_amount = float(input("Enter total bill amount: "))
+paid_amount = float(input("Enter amount paid by customer: "))
 
-t = turtle.Turtle()
-
-for i in range(4):
-    t.forward(100)
-    t.right(90)
-
-turtle.done()
+if paid_amount < total_amount:
+    due_amount = total_amount - paid_amount
+    print("Due Amount =", due_amount)
+else:
+    print("No Due Amount")
